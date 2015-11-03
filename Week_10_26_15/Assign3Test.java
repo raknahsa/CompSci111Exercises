bfed2addd1f5a248b9150b2f65858052cc208e31
Raknahsa <raknahsa@gmail.com>
2015-11-03 13:32:52 -0500

    Add Assignment Tester
    Add a program to test the 3 classes for assignment 3.

---
+++ Week_10_26_15/Assign3Test.java
@@ -0,0 +1,21 @@
+public class Assign3Test{
+  public static void main(String args[]){
+    /*
+    System.out.println("Enter the string to be translated:");
+    String str = IO.readString();
+    System.out.println("Pig Latin translated string is " + PigLatin.translate(str));
+    */
+
+    /*
+    System.out.println("Enter the string for words to be counted.");
+    String str2 = IO.readString();
+    System.out.println("Enter the min length for words to be counted.");
+    int minLength = IO.readInt();
+    System.out.println("The number of words is " + WordCount.countWords(str2, minLength));
+    */
+
+      System.out.println("Enter the string to be compressed");
+      String str3 = IO.readString();
+      System.out.println("Compressed string is " + Compress.compress(str3));
+  }
+}
