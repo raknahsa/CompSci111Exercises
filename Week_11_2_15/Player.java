2e7f2267679c40e0f8f05d40227465e8703fe041
Raknahsa <raknahsa@gmail.com>
2015-11-11 10:26:50 -0500

    Add Player class
    Add a player class to test song and album class. Add guide line for
    effective analysis in the comments

---
+++ Week_11_2_15/Player.java
@@ -0,0 +1,21 @@
+public class Player{
+  public static void main(String[] args){
+    //Efficency Analysis
+    /*
+    1. Input size : How fast or how slow it will be become.
+    2. Define operations that contribute most to run time.
+    3. Define best case behavior, worst case behavior -> write as a f(n)
+      What minimizes run time, what maxamixes run time
+      Theta = WC, Drop all non-leading terms and constants  
+      Theta(1)
+      Theta(log of 2 (n))
+      Theta(n)
+      Theta(n^2)
+      Theta(2^n)
+    */
+
+
+    Song s = new Song("Monster", 4, "Skillet", "Alt Rock");
+
+  }
+}
