4caa340b8ef386b0ddbd6ef0b142b14ea29c8467
Raknahsa <raknahsa@gmail.com>
2015-11-19 23:58:37 -0500

    Add Method for Binary Search
    Add code which adds a method for binary search.

--- Week_11_16_15/SelectionSort.java
+++ Week_11_16_15/SelectionSort.java
@@ -1,9 +1,9 @@
 public class SelectionSort{
-
   public static void main(String [] args){
     int [] arr ={5,1,3,9,0,2,4};
 
-  //  int[] sarr = ssort(arr);
+  //  ssort(arr);
+      isort(arr);
 
     for(int i = 0; i < arr.length; i++){
       System.out.println(arr[i]);
@@ -12,30 +12,51 @@ public class SelectionSort{
 
   }
 
+  public static void ssort(int [] arr){
 
-public static void ssort(int [] arr){
+    //int smallest = arr[i];
+    int tmp;
+    int index;
 
-  //int smallest = arr[i];
-  int tmp;
-  int index;
-
-  for(int i = 0; i < arr.length -1; i++){
-    //smallest = arr[i];
-    index = i;
-    for(int j = i + 1; j < arr.length; j++){
-      if(arr[j] < arr[index]){
-        //smallest = arr[j];
-        index = j;
+    for(int i = 0; i < arr.length -1; i++){
+      //smallest = arr[i];
+      index = i;
+      for(int j = i + 1; j < arr.length; j++){
+        if(arr[j] < arr[index]){
+          //smallest = arr[j];
+          index = j;
+        }
       }
+      tmp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = tmp;
     }
-    tmp = arr[i];
-    arr[i] = arr[index];
-    arr[index] = tmp;
+  //  return arr;
   }
 
-//  return arr;
+  public static void isort(int [] arr){
 
-}
+    int tmp;
+//    int index;
+
+    for(int i = 0; i < arr.length - 1; i++){
+      //smallest = arr[i];
+      for(int j = i + 1; j > 0; j--){
+        if(arr[j] < arr[j - 1]){
+          //smallest = arr[j];
+          tmp = arr[j - 1];
+          arr[j - 1] = arr[j];
+          arr[j] = tmp;
+        }
+        else{
+          break;
+        }
+      }
+    }
+
+  }
+
+
 
 
 }
